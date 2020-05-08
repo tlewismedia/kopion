e9ae764f7cc3b99060b14be48e2d67b228aed8bd
Tom <tlewismedia@gmail.com>
2020-05-08 09:16:18 -0700

    may 2020 updates

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,26 @@
-// JavaScript Document
 function GetCurrentPageName() { 
-//method to get Current page name from url. 
-var PageURL = document.location.href; 
-var PageName = PageURL.substring(PageURL.lastIndexOf('/') + 1); 
+  var PageURL = document.location.href; 
+  var PageName = PageURL.substring(PageURL.lastIndexOf('/') + 1); 
 
-return PageName.toLowerCase() ;
+  return PageName.toLowerCase() ;
 }
 
-$(document).ready(function(){	
+
 var CurrPage = GetCurrentPageName();
 
 switch(CurrPage){
-case 'index.html':
- $('#li_home').addClass('active') ;
- break;
-case 'team.html':
- $('#li_about').addClass('active') ;
- break;
-case 'contact.html':
- $('#li_contact').addClass('active') ;
- break;
- 
+  case 'index.html':
+     
+     document.querySelector('#li_home').classList.add('active') ;
+     break;
+  case 'team.html':
+     document.querySelector('#li_team').classList.add('active') ;
+     break;
+  case 'contact.html':
+     document.querySelector('#li_contact').classList.add('active') ;
+     break;
+  case 'disclosure.html':
+     document.querySelector('#li_crs').classList.add('active') ;
+     break;
+   
 }
-});
